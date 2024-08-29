9eea9cc9dea9815ea168a5c7919f92883ccd33f1
Ricch.dev <anekeaustin@gmail.com>
2024-08-29 20:59:23 +0100

    Refactor HTML and JavaScript code to separate event listeners from inline code

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ and click add it will display it on page instead of the console as in pratice 2*
  renderTodoList()
 
  /*const renderTodoList = () => {
-
  };*/
 function renderTodoList() { // i left this on a regular function syntax because it is easier to read and Hoisting(which means we can call this function before we create it)
 
@@ -35,13 +34,10 @@ function renderTodoList() { // i left this on a regular function syntax because 
         const html = `
         <div>${name}</div>
         <div>${dueDate}</div>
-          <button onclick="
-            todoList.splice(${index}, 1);
-            renderTodoList(); 
-          " class="delete-todo-button">Delete</button>
+          <button class="delete-todo-button js-delete-button">Delete</button>
         `; // this technique is called generating the HTML
         todoListHTML += html; // Add the HTML to the todoListHTML
-      })
+      });
 
       //this below is the for loop method of looping through an array
       /*for (let i = 0; i < todoList.length; i++) {
@@ -62,7 +58,28 @@ function renderTodoList() { // i left this on a regular function syntax because 
 
 
       document.querySelector('.js-todo-list').innerHTML = todoListHTML; // Display the todoListHTML in the webpage
-}
+
+/*below we used querySelectorAll to get a list of all the delete butoon on the page
+  and then we looped through the list using forEach()
+  and for each delete button we added a 'click' eventlistener
+  and ran the same code that we did before.
+*/ 
+      document.querySelectorAll('.js-delete-button')
+       .forEach((deleteButton, index) => {
+          deleteButton.addEventListener('click', () => {
+            todoList.splice(index, 1); // Remove the todo from the array
+            renderTodoList(); // Update the webpage to display the remaining todos
+          });
+       });
+
+}     
+
+
+
+   document.querySelector('.js-add-button')
+   .addEventListener('click', () => {
+    addTodo(); // Call the function when we click "Add"
+  }); // Add event listener to the "Add" button
 
 function addTodo() {  // When we click "Add",
   const inputElement = document.querySelector('.js-name-input');
